b040c31c5c0aaa991a1b2a3327ff408e4095b3fb
Mustafa Saifee <mustafa.saifee@aiven.io>
2022-09-15 03:49:36 +0300

    Adding UNDP design styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,167 +1,86 @@
-import { createGlobalStyle } from 'styled-components';
-import { Spin } from 'antd';
+import {
+  Checkbox, Input, Radio, Select, Tabs,
+} from 'antd';
 import 'antd/dist/antd.css';
-
-const GlobalStyle = createGlobalStyle`
-  :root {
-    --white: #FFFFFF;
-    --primary-blue: #006EB5;
-    --blue-medium: #4F95DD;
-    --blue-bg: #94C4F5;
-    --navy: #082753;
-    --black-100: #FAFAFA;
-    --black-200: #f5f9fe;
-    --black-300: #EDEFF0;
-    --black-400: #E9ECF6;
-    --black-450: #DDD;
-    --black-500: #A9B1B7;
-    --black-550: #666666;
-    --black-600: #212121;
-    --black-700: #000000;
-    --blue-very-light: #F2F7FF;
-    --yellow: #FBC412;
-    --yellow-bg: #FFE17E;
-    --red: #D12800;
-    --red-bg: #FFBCB7;
-    --shadow:0px 10px 30px -10px rgb(9 105 250 / 15%);
-    --shadow-bottom: 0 10px 13px -3px rgb(9 105 250 / 5%);
-    --shadow-top: 0 -10px 13px -3px rgb(9 105 250 / 15%);
-    --shadow-right: 10px 0px 13px -3px rgb(9 105 250 / 5%);
-    --shadow-left: -10px 0px 13px -3px rgb(9 105 250 / 15%);
-  }
-  
-  html { 
-    font-size: 62.5%; 
-  }
-
-  .react-dropdown-select-option{
-    color:var(--black) !important;
-    background-color:var(--primary-color-light) !important;
-  }
-  .react-dropdown-select-option-label, .react-dropdown-select-option-remove{
-    font-weight: 400;
-    background-color:var(--primary-color-light);
-    padding: 0.5rem;
-  }
-
-  body {
-    font-family: "proxima-nova", "Helvetica Neue", "sans-serif";
-    color: var(--black-600);
-    background-color: var(--white);
-    margin: 0;
-    padding: 1rem 0;
-    font-size: 1.6rem;
-    font-weight: normal;
-    line-height: 2.56rem;
-  }
-
-  a {
-    text-decoration: none;
-    color: var(--primary-blue);
-  }
-
-  h1 {
-    color: var(--primary-blue);
-    font-size: 3.2rem;
-    font-weight: 700;
-    
-    @media (max-width: 760px) {
-      font-size: 2.4rem;
-    }
-    @media (max-width: 480px) {
-      font-size: 1.8rem;
-    }
-  }
-  
-  button.primary {
-    border-radius: 0.2rem !important;
-    font-size: 1.4rem !important;
-    font-weight: normal !important;
-    color: var(--white) !important;
-    background-color: var(--primary-blue) !important;
-    border: 1px solid var(--primary-blue) !important;
-    cursor: pointer !important;
-    padding: 0.4rem 1rem !important;
-    &:hover {
-      border: 1px solid var(--blue-medium) !important;
-      background-color: var(--blue-medium) !important;
-    }
-    &:active{
-      border: 1px solid var(--blue-medium) !important;
-      background-color: var(--blue-medium) !important;
-    }
-  }
-
-  button.secondary {
-    border-radius: 0.2rem !important;
-    font-size: 1.4rem !important;
-    font-weight: normal !important;
-    color: var(--black-600) !important;
-    border: 1px solid var(--black-450) !important;
-    cursor: pointer !important;
-    padding: 0.4rem 1rem !important;
-    background-color: var(--white) !important;
-    &:hover {
-      border: 1px solid var(--primary-blue) !important;
-      color: var(--primary-blue) !important;
-    }
-    &:active{
-      border: 1px solid var(--primary-blue) !important;
-      color: var(--primary-blue) !important;
-    }
-  }
-
-  a:hover {
-    font-weight: bold;
-  }
-
-  .bold{
-    font-weight: 700;
-  }
-  
-  .italics{
-    font-style: italic;
-  }
-
-  .ant-modal-close {
-    display: none !important;
-  }
-
-  .ant-select-item-option-content {
-    white-space: normal;
-  }
-
-  .ant-select-selector {
-    border-radius: 0.5rem !important;
-    background-color: var(--black-200) !important;
-  }
-  .ant-slider-mark-text {
-    font-size: 1rem !important;
-    display: none;
-    &:first-of-type {
-      display: inline;
-    }
-    &:last-of-type {
-      display: inline;
-    }
-  }
-  .ant-slider-tooltip{
-    padding: 0 !important;
-  }
-  .ant-tooltip-inner{
-    font-size: 1.4rem !important;
-    background-color: var(--black-550) !important;
-    border-radius: 0.4rem;
-  }
-  .ant-tooltip-arrow-content{
-    background-color: var(--black-550) !important;
-  }
-`;
+import './style/style.css';
+import './style/tabStyle.css';
+import './style/checkboxStyle.css';
+import './style/radioStyle.css';
+import './style/selectStyle.css';
+import './style/inputStyle.css';
 
 const App = () => (
   <>
-    <GlobalStyle />
-    <Spin size='large' />
+    <h1>UNDP Data Future Platform Template</h1>
+    <div style={{
+      display: 'flex', gap: '2rem', flexWrap: 'wrap', alignItems: 'center', marginBottom: '2rem',
+    }}
+    >
+      <button type='button' className='button button-primary button-arrow'>Read More</button>
+      <button type='button' className='button button-secondary button-arrow'>Read More</button>
+      <button type='button' className='button button-primary disabled button-arrow'>Read More</button>
+      <button type='button' className='button button-tertiary button-arrow'>Read More</button>
+      <button type='button' className='button button-tertiary disabled button-arrow'>Read More</button>
+    </div>
+    <div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    </div>
+    <div style={{ width: '25%' }}>
+      <div className='stat-card'>
+        <h2>832</h2>
+        <h4>Million</h4>
+        <p>Lorem Ipsum dolor sit Amet</p>
+      </div>
+    </div>
+    <a href='www.google.com'>Hello World</a>
+
+    <Select
+      className='undp-select'
+      style={{ width: '30%' }}
+      allowClear
+      showSearch
+      placeholder='Select All'
+      dropdownMatchSelectWidth
+      clearIcon={<div className='clearIcon' />}
+    >
+      <Select.Option className='undp-select-option' value='jack'>Jack</Select.Option>
+      <Select.Option className='undp-select-option' value='lucy'>Lucy</Select.Option>
+      <Select.Option className='undp-select-option' value='disabled' disabled>
+        Disabled
+      </Select.Option>
+      <Select.Option className='undp-select-option' value='Yiminghe'>yiminghe</Select.Option>
+    </Select>
+
+    <Tabs
+      defaultActiveKey='1'
+      className='undp-tabs'
+    >
+      <Tabs.TabPane className='undp-tab-content' tab='Tab 1' key='1'>
+        Content of Tab Pane 1
+      </Tabs.TabPane>
+      <Tabs.TabPane className='undp-tab-content' tab='Tab 2' key='2'>
+        Content of Tab Pane 2
+      </Tabs.TabPane>
+      <Tabs.TabPane className='undp-tab-content' tab='Tab 3' key='3'>
+        Content of Tab Pane 3
+      </Tabs.TabPane>
+    </Tabs>
+
+    <Checkbox className='undp-checkbox' checked>Checkbox Check</Checkbox>
+    <Checkbox className='undp-checkbox' checked={false}>Checkbox Unchecked</Checkbox>
+    <Checkbox className='undp-checkbox' disabled>Checkbox Unchecked</Checkbox>
+    <Radio.Group value={1}>
+      <Radio className='undp-radio' value={1}>A</Radio>
+      <Radio className='undp-radio' value={2}>B</Radio>
+      <Radio className='undp-radio' value={3}>C</Radio>
+      <Radio className='undp-radio' disabled value={4}>D</Radio>
+    </Radio.Group>
+    <Input style={{ width: '30%', margin: '2rem' }} className='undp-input' placeholder='Basic usage' />
   </>
 );
 
