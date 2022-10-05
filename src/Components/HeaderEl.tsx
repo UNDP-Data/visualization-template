cf0c5c024ea455af2443238b50611432360bc8f1
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2022-10-05 13:45:07 +0300

    add header

--- src/Components/HeaderEl.tsx
+++ src/Components/HeaderEl.tsx
@@ -7,8 +7,8 @@ export const HeaderEl = () => (
         <div className='flex-div flex-vert-align-center'>
           <img src='https://design.undp.org/static/media/undp-logo-blue.4f32e17f.svg' alt='UNDP Logo' />
           <div className='undp-site-title'>
-            <span>Data Futures Platform</span>
-            <span>Design Style Guidelines</span>
+            <span><a href='https://data.undp.org/' target='_blank' rel='noreferrer'>Data Futures Platform</a></span>
+            <span><a href='/'>Design Style Guidelines</a></span>
           </div>
         </div>
       </div>
