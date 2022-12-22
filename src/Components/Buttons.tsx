f84aad0923075aa20a0e72c9b45a8717a6d908be
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2022-12-22 09:09:24 +0200

    Add accessibility and focus

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -2,10 +2,10 @@ import '../style/buttonStyle.css';
 
 export const Buttons = () => (
   <div className='flex-div flex-vert-align-center flex-wrap margin-bottom-07'>
-    <button type='button' className='undp-button button-primary button-arrow'>Read More</button>
-    <button type='button' className='undp-button button-secondary button-arrow'>Read More</button>
-    <button type='button' className='undp-button button-primary disabled button-arrow'>Read More</button>
-    <button type='button' className='undp-button button-tertiary button-arrow'>Read More</button>
-    <button type='button' className='undp-button button-tertiary disabled button-arrow'>Read More</button>
+    <button type='button' aria-label='learn more about something' className='undp-button button-primary button-arrow'>Read More</button>
+    <button type='button' aria-label='learn more about something' className='undp-button button-secondary button-arrow'>Read More</button>
+    <button type='button' aria-label='learn more about something' className='undp-button button-primary disabled button-arrow'>Read More</button>
+    <button type='button' aria-label='learn more about something' className='undp-button button-tertiary button-arrow'>Read More</button>
+    <button type='button' aria-label='learn more about something' className='undp-button button-tertiary disabled button-arrow'>Read More</button>
   </div>
 );
